e0ac56891aef439283fe023c81165fa615386430
Royaltechsis <osenibunmi2023@gmail.com>
2024-11-09 15:06:57 +0100

    boiler plate complete

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>this project includes firebase,  shacdn ui, and tailwind css</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
